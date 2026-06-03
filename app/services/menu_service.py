828b6bb72e6deb6aebd243edea1e807bb6fc542b
MinJun916 <ed125248@gmail.com>
2026-06-03 16:58:58 +0900

    refactor: simplify menu retrieval by removing offset pagination and updating related service and router methods

--- app/services/menu_service.py
+++ app/services/menu_service.py
@@ -4,22 +4,26 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.menu_crud import (
     create_menu_crud,
-    find_displayed_menus_with_offset,
+    find_displayed_menus_crud,
     soft_delete_menu_crud,
     update_menu_crud,
 )
 from app.schemas.menu_schema import (
     CreateMenuRequest,
-    MenuOffsetFilterData,
     UpdateMenuRequest,
 )
 
+# async def get_displayed_menus_with_offset(
+#     db: AsyncSession,
+#     filter_data: MenuOffsetFilterData,
+# ):
+#     return await find_displayed_menus_with_offset(db, filter_data)
 
-async def get_displayed_menus_with_offset(
+
+async def get_displayed_menus(
     db: AsyncSession,
-    filter_data: MenuOffsetFilterData,
 ):
-    return await find_displayed_menus_with_offset(db, filter_data)
+    return await find_displayed_menus_crud(db)
 
 
 async def create_menu(
